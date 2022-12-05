a80114121708815310ac1881d5e1d218f8c337ed
Carlos Fournery <carlosgfc@gmail.com>
2022-12-05 19:08:22 -0300

    mvt primer entrega  de mi proyecto

---
+++ seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="Carlos", direccion="Cordoba 885", numero_pasaporte=123123).save()
+Familiar(nombre="Alberto", direccion="Monteagudo 780", numero_pasaporte=890890).save()
+Familiar(nombre="Eugenia", direccion="Hernandarias 1098", numero_pasaporte=345345).save()
+Familiar(nombre="Yvonne", direccion="Julian Alvarez 585", numero_pasaporte=567567).save()
+print("Se cargo con éxito los usuarios de pruebas")
