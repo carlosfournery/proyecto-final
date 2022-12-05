8aa048c553af740cf77c75ea266d9c1dfd95995c
Carlos Fournery <carlosgfc@gmail.com>
2022-12-05 16:02:01 -0300

    primer commit de mi proyecto

---
+++ ejemplo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "ejemplo/saludar.html")
+
+def saludar_a(request):
+    nombre = "German"
+    return render(request, 
+    "ejemplo/saludar_a.html",
+    {"nombre":nombre})
